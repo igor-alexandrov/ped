6dd01f9e46ea1f54cb5bbafc1d32c9c06b82edde
Igor Alexandrov <igor.alexandrov@gmail.com>
2021-01-19 15:06:43 +0300

    Added specs

--- spec/ped_spec.rb
+++ spec/ped_spec.rb
@@ -5,7 +5,21 @@ RSpec.describe Ped do
     expect(Ped::VERSION).not_to be nil
   end
 
-  it 'does something useful' do
-    expect(false).to eq(true)
+  describe '#public_email_domain?' do
+    it 'returns true for public domain' do
+      expect(Ped.public_email_domain?('gmail.com')).to be_truthy
+    end
+
+    it 'returns false for corporate domain' do
+      expect(Ped.public_email_domain?('jetrockets.pro')).to be_falsy
+    end
+
+    it 'returns false for blank string' do
+      expect(Ped.public_email_domain?('')).to be_falsy
+    end
+
+    it 'returns false for nil' do
+      expect(Ped.public_email_domain?(nil)).to be_falsy
+    end
   end
 end
